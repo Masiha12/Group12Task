f26585be80bbe05f762b364770166f049b68e09c
Masiha12 <abdullahmasiha44@gmail.com>
2022-09-28 20:36:29 -0400

    first commit

---
+++ src/FinalProject/finalClass.java
@@ -0,0 +1,10 @@
+package FinalProject;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("This is the code for the mercedes");
+	}
+
+}
